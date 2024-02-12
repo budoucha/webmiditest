c6b473ca1773154874043daa6aec75a2010aa880
budoucha <budoucha@gmail.com>
2024-02-13 03:47:12 +0900

    rich output mode

--- script.js
+++ script.js
@@ -20,6 +20,14 @@ console.log = (...msg) => {
     outputElement.scrollTop = outputElement.scrollHeight;
 };
 
+// mode switch
+const modeSwitch = document.getElementById('richMode');
+modeSwitch.addEventListener('change', (event) => {
+    richMode = event.target.checked;
+});
+let richMode = modeSwitch.checked;
+
+
 const onMIDISuccess = (midiAccess) => {
     const inputs = midiAccess.inputs.values();
     for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
@@ -32,17 +40,72 @@ const onMIDIFailure = () => {
     console.log("Failed to get MIDI access.");
 }
 
+const outputRich = (data) => {
+    // pitch bend
+    if (data[0] == 224) {
+        const pitch = (data[2] << 7) + data[1];
+        console.log(`pitch: ${pitch}`);
+        return;
+    }
+    // control change
+    if (data[0] == 176) {
+        const control = data[1];
+        const value = data[2];
+        console.log(`control: ${control},\tvalue: ${value}`);
+        return;
+    }
+    // key press
+    if (data[0] == 144) {
+        const note = data[1];
+        const octave = Math.floor(note / 12) - 1;
+        const noteName = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][note % 12];
+        const velocity = (data.length > 2) ? data[2] : 0;
+        console.log(`${note}(${noteName}${octave}),\tvelocity: ${velocity}`);
+        return;
+    }
+    // key release
+    if (data[0] == 128) {
+        const note = data[1];
+        const octave = Math.floor(note / 12) - 1;
+        const noteName = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][note % 12];
+        console.log(`released ${note}(${noteName}${octave})`);
+        return;
+    }
+    // pad press
+    if (data[0] == 153) {
+        const pad = data[1];
+        const velocity = (data.length > 2) ? data[2] : 0;
+        console.log(`pad: ${pad},\tvelocity: ${velocity}`);
+        return;
+    }
+    // pad release
+    if (data[0] == 137) {
+        const pad = data[1];
+        console.log(`released pad: ${pad}`);
+        return;
+    }
+    // pad velocity change?
+    if (data[0] == 217) {
+        const velocity = data[1];
+        console.log(`pad velocity: ${velocity}`);
+        return;
+    }
+    // other
+    console.log(...data);
+}
+
 const onMIDIMessage = (message) => {
     const data = message.data;
-    const command = data[0]; // ↑↓:176 , ←→: 224
-    const note = data[1];
-    const velocity = (data.length > 2) ? data[2] : 0;
-    console.log(`command: ${command},\tnote: ${note},\tvelocity: ${velocity}`);
+    if (richMode) {
+        outputRich(data);
+        return;
+    }
+    console.log(...data);
 }
 
 const onMIDIConnectionChange = (event) => {
     if (event.port.type !== "input") return;
-    const name = event.port.name?? null;
+    const name = event.port.name ?? null;
     console.log("MIDI device:", name);
 }
 
